345ad653c73fe5aaf63ea37bfc0489f9a7be084a
admin@codexorient.com <admin@codexorient.com>
2021-02-12 19:25:51 +0545

    small change in home page

--- DateHack/index.php
+++ DateHack/index.php
@@ -48,7 +48,7 @@
 
 		<div class="ab-card">
 			<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="<?= base_url?>images/background.svg" alt="Card image cap">
+				<img class="card-img-top" src="<?= base_url?>images/background.jpg" alt="Card image cap">
 				<div class="card-body">
     				<h5 class="card-title">Card title</h5>
     				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -78,35 +78,33 @@
 	<div id="services">
 		<div class="upper-line"></div>
 		<div class="ab-text">Our Happy Users</div>
-
-		<div class="service-carousel">
-			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-     Hello
-    </div>
-    <div class="carousel-item">
-      hi
-    </div>
-    <div class="carousel-item">
-     this
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+		<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					Hi
+				</div>
+				<div class="carousel-item">
+					Namestry
+				</div>
+				<div class="carousel-item">
+					hello
+				</div>
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
 		</div>
+
+
 	</div>
 	<!-- End of services -->
 
 	<!-- script -->
-	<script type="text/javascript" src="<?= base_url?>assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="<?= base_url?>assets/bootstrap/js/bootstrap.min.js"></script>
 	<!-- end script -->
 </body>
