babc3ecce56badbf45c4f16b1585a834b6a3d7fe
unknown <admin@codexorient.com>
2021-02-12 14:19:19 +0545

    Basic Code

---
+++ DateHack/autoLoader.php
@@ -0,0 +1,14 @@
+<?php
+spl_autoload_register('AutoLoadeClasses');
+
+function AutoLoadeClasses($name){
+	$file = __DIR__.'\\';
+	include $file.$name.".php";
+	
+}
+$url = $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+$sep_url = explode("/", $url);
+$base_url = 'http://'.$_SERVER['HTTP_HOST'].'/'.$sep_url[1].'/'.$sep_url[2].'/'.$sep_url[3].'/';
+define('base_url', $base_url);
+
+?>
---
+++ DateHack/index.php
@@ -0,0 +1,40 @@
+<?php
+	include_once 'autoLoader.php';  
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DateHack:Now It's Your Time To Mingle</title>
+	<!-- meta tags -->
+	<meta charset="UTF-8">
+  	<meta name="description" content="This is Hackthon DateHack Web Application designed and developed by Team CodexOrient Nami College">
+  	<meta name="keywords" content="DateHack, Team CodexOrient, Search your date patner">
+  	<meta name="author" content="Team Codex Orient">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- end of meta tags -->
+
+	<!-- links -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url?>assets/css/style.css">
+	<!-- end links  -->
+</head>
+<body>
+	<!-- header -->
+	<header class="d-flex">
+		<div class="img-logo"><img src="<?= base_url?>/images/app/logo.png" alt="Datemandu Logo"></div>
+		<div class="head-wrapper d-flex">
+			<div class="login-wrapper">Login</div>
+			<div class="register-wrapper">Register</div>
+		</div>
+	</header>
+	<!-- End of Header -->
+
+	<!-- Section -->
+	<section class="body-main">
+		
+	</section>
+	<div class="main-container"></div>
+	<!-- End of section -->
+</body>
+</html>
